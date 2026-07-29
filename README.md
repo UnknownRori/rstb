a5c968bf13adff30d125d6c2983c26ff37321ce3
UnknownRori <unknownrori@proton.me>
2026-07-29 19:42:46 +0700

    update: string builder

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ Some of the header uses other header to avoid duplication.
 |**[rstb_arena.h](rstb_da.h)**    |0.1           |misc    |337|Simple to use arena (bump) allocator|
 |**[rstb_sv.h](rstb_sv.h)**       |0.1           |misc    |200|Simple string manipulation helper   |
 |**[rstb_iface.h](rstb_iface.h)** |0.0           |misc    |90 |Simple vtable dispatch              |
-|**[rstb_sb.h](rstb_sb.h)**       |0.0           |misc    |96 |Simple to use String Builder        |
+|**[rstb_sb.h](rstb_sb.h)**       |0.0           |misc    |112|Simple to use String Builder        |
 
 ## Usage
 
--- rstb_sb.h
+++ rstb_sb.h
@@ -11,7 +11,7 @@
 int main()
 {
     rstb_sb sb = {0};
-    rstb_sb_init(&sb, 64);
+    rstb_sb_reserve(&sb, 64);
     rstb_sb_append(&sb, "Hello: ");
     rstb_sb_appendf(&sb, "%d\n", 69);
     printf("%s", sb.items);
@@ -33,7 +33,6 @@ int main()
     #define RSTB_SB_API
 #endif
 
-#include "rstb_da.h"
 #include <stdio.h>
 #include <stdarg.h>
 #include <string.h>
@@ -45,18 +44,20 @@ rstb_da_decl(char, rstb_sb);
 extern "C" {  
 #endif
 
-RSTB_SB_API void rstb_sb_init(rstb_sb* self, size_t reserve);
+RSTB_SB_API void rstb_sb_reserve(rstb_sb* self, size_t reserve);
 RSTB_SB_API void rstb_sb_reset(rstb_sb* self);
 RSTB_SB_API void rstb_sb_free(rstb_sb* self);
 RSTB_SB_API int rstb_sb_append(rstb_sb* self, const char* str);
 RSTB_SB_API int rstb_sb_appendf(rstb_sb* self, const char* format, ...);
+RSTB_SB_API int rstb_sb_vsappendf(rstb_sb* self, const char* format, va_list args);
 
 #if defined(__cplusplus)
 }
 #endif // __cplusplus
 
 #ifdef RSTB_SB_IMPLEMENTATION
-RSTB_SB_API void rstb_sb_init(rstb_sb* self, size_t reserve) { rstb_da_reserve(self, reserve); }
+// #if 1
+RSTB_SB_API void rstb_sb_reserve(rstb_sb* self, size_t reserve) { rstb_da_reserve(self, reserve); }
 RSTB_SB_API void rstb_sb_reset(rstb_sb* self) { rstb_sb_reset(self); }
 RSTB_SB_API void rstb_sb_free(rstb_sb* self) { rstb_sb_free(self); }
 RSTB_SB_API int rstb_sb_append(rstb_sb* self, const char* str)
@@ -65,10 +66,8 @@ RSTB_SB_API int rstb_sb_append(rstb_sb* self, const char* str)
     rstb_da_append_many(self, str, len);
     return len;
 }
-RSTB_SB_API int rstb_sb_appendf(rstb_sb* self, const char* format, ...)
+RSTB_SB_API int rstb_sb_vsappendf(rstb_sb* self, const char* format, va_list args)
 {
-    va_list args;
-    va_start(args, format);
     va_list copy;
     va_copy(copy, args);
     int needed = vsnprintf(NULL, 0, format, copy);
@@ -87,10 +86,27 @@ RSTB_SB_API int rstb_sb_appendf(rstb_sb* self, const char* format, ...)
         args);
     
     self->count += (size_t)needed;
+
+    return needed;
+}
+RSTB_SB_API int rstb_sb_appendf(rstb_sb* self, const char* format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    int needed = rstb_sb_vsappendf(self, format, args);
     va_end(args);
 
     return needed;
 }
+#endif
+
+#ifdef RSTB_SB_STRIP_PREFIX
+#define sb_init(...)   rstb_sb_init(__VA_ARGS__)
+#define sb_reserve(...)   rstb_sb_reserve(__VA_ARGS__)
+#define sb_free(...)   rstb_sb_free(__VA_ARGS__)
+#define sb_append(...)     rstb_sb_append(__VA_ARGS__)
+#define sb_appendf(...)     rstb_sb_appendf(__VA_ARGS__)
+#define sb_vsappendf(...)   rstb_sb_vsappendf(__VA_ARGS__)
 #endif
 
 #endif //  RSTB_SB_H
